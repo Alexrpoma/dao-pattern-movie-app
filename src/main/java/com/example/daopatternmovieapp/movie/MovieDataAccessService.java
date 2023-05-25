f348c068903cb5ccf6cb317e175c111c06bce4bf
Alex Poma <alex.rpoma@outlook.com>
2023-05-25 12:21:32 -0400

    PostMovie, GetMovies, RowMapper implemented.

--- src/main/java/com/example/daopatternmovieapp/movie/MovieDataAccessService.java
+++ src/main/java/com/example/daopatternmovieapp/movie/MovieDataAccessService.java
@@ -1,5 +1,6 @@
 package com.example.daopatternmovieapp.movie;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -7,14 +8,27 @@ import java.util.Optional;
 
 @Repository
 public class MovieDataAccessService implements MovieDAO{
+
+  private final JdbcTemplate jdbcTemplate;
+
+  public MovieDataAccessService(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
   @Override
   public List<Movie> selectMovies() {
-    throw new UnsupportedOperationException("not implemented");
+    String sql = """
+        SELECT id, name, release_date FROM movie LIMIT 100;
+      """;
+    return jdbcTemplate.query(sql, new MovieRowMapper());
   }
 
   @Override
   public int insertMovie(Movie movie) {
-    throw new UnsupportedOperationException("not implemented");
+    String sql = """
+        INSERT INTO movie(name, release_date) VALUES(?, ?);
+      """;
+    return jdbcTemplate.update(sql, movie.name(), movie.releaseDate());
   }
 
   @Override
